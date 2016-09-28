264e6a71b46fc6ef91118c2bd426d20e13b15827
Ram Paul <ram@edutechsolutionsbd.com>
2016-09-28 12:01:05 +0600

    changes happened in slider speed and price circle

--- app/views/front/header.blade.php
+++ app/views/front/header.blade.php
@@ -53,7 +53,7 @@
                 slidesToShow: 1,
                 slidesToScroll: 1,
                 autoplay: true,
-                autoplaySpeed: 2000,
+                autoplaySpeed: 6000,
                 mobileFirst: true,
                 //fade: true,
                 focusOnSelect: true,
