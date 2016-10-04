6877f3d353fffff687cfa0d90b529bc0c4c1665d
Ram Paul <ram@edutechsolutionsbd.com>
2016-10-04 17:52:29 +0600

    In waynaperu new admin theme/template has been created

---
+++ app/views/admin/theme/header.blade.php
@@ -0,0 +1,31 @@
+
+    <title>Half-Way</title>
+
+    <!-- BEGIN META -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="your,keywords">
+    <meta name="description" content="Short explanation about this website">
+    <!-- END META -->
+
+    <!-- BEGIN STYLESHEETS -->
+    <link href='http://fonts.googleapis.com/css?family=Roboto:300italic,400italic,300,400,500,700,900' rel='stylesheet' type='text/css'/>
+    <link href="{{ asset('assets/admin/css/theme-default/bootstrap.css?1422792965') }}" rel="stylesheet">
+    <link href="{{ asset('assets/admin/css/theme-default/materialadmin.css?1425466319') }}" rel="stylesheet">
+    <link href="{{ asset('assets/admin/css/theme-default/font-awesome.min.css?1422529194') }}" rel="stylesheet">
+    <link href="{{ asset('assets/admin/css/theme-default/material-design-iconic-font.min.css?1421434286') }}" rel="stylesheet">
+    <link href="{{ asset('assets/admin/css/theme-default/libs/rickshaw/rickshaw.css?1422792967') }}" rel="stylesheet">
+    <link href="{{ asset('assets/admin/css/theme-default/libs/morris/morris.core.css?1420463396') }}" rel="stylesheet">
+    {{--<link type="text/css" rel="stylesheet" href="../../assets/css/theme-default/bootstrap.css?1422792965" />--}}
+    {{--<link type="text/css" rel="stylesheet" href="../../assets/css/theme-default/materialadmin.css?1425466319" />--}}
+    {{--<link type="text/css" rel="stylesheet" href="../../assets/css/theme-default/font-awesome.min.css?1422529194" />--}}
+    {{--<link type="text/css" rel="stylesheet" href="../../assets/css/theme-default/material-design-iconic-font.min.css?1421434286" />--}}
+    {{--<link type="text/css" rel="stylesheet" href="../../assets/css/theme-default/libs/rickshaw/rickshaw.css?1422792967" />--}}
+    {{--<link type="text/css" rel="stylesheet" href="../../assets/css/theme-default/libs/morris/morris.core.css?1420463396" />--}}
+            <!-- END STYLESHEETS -->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script type="text/javascript" src="{{ asset('assets/admin/js/libs/utils/html5shiv.js?1403934957') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/admin/js/libs/utils/respond.min.js?1403934956') }}"></script>
+    <![endif]-->
---
+++ app/views/admin/theme/master.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('admin.theme.header')
+</head>
+
+<body class="menubar-hoverable header-fixed ">
+
+    @yield('content')
+    @include('admin.theme.footer')
+
+</body>
+</html>
+
