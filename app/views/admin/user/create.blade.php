b1c15b7df322923f61e19970c4b6f692822942f0
Ram Paul <ram@edutechsolutionsbd.com>
2016-10-06 16:01:20 +0600

    06-10-2016(8):: custom_styles.css has been updated

--- app/views/admin/user/create.blade.php
+++ app/views/admin/user/create.blade.php
@@ -1,33 +1,42 @@
 @extends('admin.layout')
 @section('content')
 
-        <div class="col-md-12">
+        <div class="col-md-6 col-lg-8 col-sm-6">
             <div class="card">
                 <div class="card-body">
                     <div class="box-login">
                         {{ Form::open(array('url' => 'user/store','class' => 'form-horizontal','files' =>true)) }}
+
                         @if(Session::has('message'))
                             <div class="alert alert-info">
                                 {{ Session::get('message') }}
                             </div>
                         @endif
 
-                            <div class="col-md-8 col-lg-8 col-sm-12">
-                                <div class="panel panel-primary">
-                                    <div class="panel-heading">Add New User</div>
-                                    <div class="panel-body">
+                        <div class="tabbable-panel">
+                            <div class="tabbable-line">
+                                <ul class="nav nav-tabs ">
+                                    <li class="active">
+                                        <a href="#" data-toggle="tab"> Add New User </a>
+                                    </li>
+                                </ul>
+                                <div class="tab-content">
+                                    <div class="tab-pane active " id="">
                                         @include('admin.user._form')
-
-                                        <div class="form-actions">
+                                        {{--<div class="form-actions">
                                             {{ Form::submit('Save', $attributes = ['class' => 'btn btn-primary pull-left']) }}
                                             &nbsp;&nbsp;
                                             <a href="{{ URL::previous() }}" class="btn btn-info">Back</a>
-                                        </div>
+                                        </div>--}}
                                     </div>
                                 </div>
                             </div>
+                        </div>
 
-
+                        <div class="form-actions pull-right">
+                            &nbsp;&nbsp;<a href="{{ URL::previous() }}" class="btn btn-info">Back</a>
+                            {{ Form::submit('Save', $attributes = ['class' => 'btn btn-green']) }}
+                        </div>
 
                         {{ Form::close() }}
                     </div>
--- app/views/admin/user/edit.blade.php
+++ app/views/admin/user/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.layout')
 @section('content')
 
-        <div class="col-md-12">
+        {{--<div class="col-md-12">
             <div class="card">
                 <div class="card-body">
                     <div class="box-login">
@@ -29,9 +29,48 @@
                     </div>
                 </div>
             </div>
+        </div>--}}
+
+
+
+        <div class="col-md-6 col-lg-8 col-sm-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="box-login">
+                        {{ Form::model($user,array('url' => 'user/update/'.$user->id,'class' => 'form-horizontal','files' =>true, 'id'=>'chkpass')) }}
+
+                        @if(Session::has('message'))
+                            <div class="alert alert-info">
+                                {{ Session::get('message') }}
+                            </div>
+                        @endif
+
+                        <div class="tabbable-panel">
+                            <div class="tabbable-line">
+                                <ul class="nav nav-tabs ">
+                                    <li class="active">
+                                        <a href="#" data-toggle="tab"> Edit User </a>
+                                    </li>
+                                </ul>
+                                <div class="tab-content">
+                                    <div class="tab-pane active " id="">
+                                        @include('admin.user._form')
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-actions pull-right">
+                            &nbsp;&nbsp;<a href="{{ URL::previous() }}" class="btn btn-info">Back</a>
+                            {{ Form::submit('Save', $attributes = ['class' => 'btn btn-green']) }}
+                        </div>
+
+                        {{ Form::close() }}
+                    </div>
+                </div>
+            </div>
         </div>
 
-        <body>
 
         <script>
             jQuery.validator.setDefaults({
@@ -45,7 +84,11 @@
                         equalTo: "#password"
                     }
                 },
-                messages:{ c_password:'<div style="margin-top: 20px; color:red;">Password not matched. Type again</div>'}
+                messages:
+                {
+                    password:'<div style="margin-top: 20px; color:red;">Password Required</div>',
+                    c_password:'<div style="margin-top: 20px; color:red;">Password not matched. Type again</div>'
+                }
             });
         </script>
 
