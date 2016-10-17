4e4d023c2635693177ec8f7340a270ebf3967e6f
Ram Paul <ram@edutechsolutionsbd.com>
2016-10-17 12:05:29 +0600

    17 Oct 2016/Waynaperu(6):: Type field is added to admin/user/_formPhone.blade

--- app/views/admin/user/_formPhone.blade.php
+++ app/views/admin/user/_formPhone.blade.php
@@ -1,8 +1,16 @@
 <div class="row">
     <div class="col-md-12">
-        <div class="form-group">
-            <label for="number">Phone Number</label>
-            {{ Form::text('number',null,['class'=>'form-control','required']) }}
+        <div class="col-md-6">
+            <div class="form-group">
+                <label for="number">Phone Number</label>
+                {{ Form::text('number',null,['class'=>'form-control','required']) }}
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="form-group">
+                <label for="number">Type</label>
+                {{ Form::select('phone_type',['1'=>'Telephone','2'=>'RPC','3'=>'RPM'],null,['class'=>'form-control']) }}
+            </div>
         </div>
     </div>
 </div>
