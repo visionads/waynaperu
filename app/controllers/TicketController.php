ff3675292abc37031a7e0bd8d3cd8930936946f6
SR <me@selimreza.com>
2016-10-25 00:31:56 +0600

    UP|

--- app/controllers/TicketController.php
+++ app/controllers/TicketController.php
@@ -57,6 +57,7 @@ class TicketController extends Controller
                       order_items.order_id,
                       users.first_name,
                       users.last_name,
+                      users.type,
                       users1.first_name as provider_first_name,
                       users1.last_name as provider_last_name,
                       users1.email as provider_email,
@@ -78,8 +79,12 @@ class TicketController extends Controller
                     GROUP BY order_items.id"));
             $data['tickets']=$x;
 
-            foreach ($data['tickets'] as $ticket) {
-//                dd($ticket);
+            foreach ($data['tickets'] as $ticket)
+            {
+
+                TicketController::html_to_jpg($ticket);
+                
+                #dd($ticket);
                 $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
                 $charactersLength = strlen($characters);
                 $randomString = '';
@@ -103,8 +108,8 @@ class TicketController extends Controller
             TicketController::sendEmail($order_id);
             $o=Order::find($order_id);
             $o->status='SUCCESS';
-//            $o->save();
-//            dd($order);
+            #$o->save();
+            #dd($order);
             if(isset($ipn) && !empty($ipn))
             {
                 return true;
@@ -126,20 +131,20 @@ class TicketController extends Controller
         ];
 
         $conv = new \Anam\PhantomMagick\Converter();
+
         if($ticket->type=='provider')
         {
-            $tnm=$ticket->ticket_number;
+            $tnm= $ticket->ticket_number;
             $ticket->ticket_number= substr($ticket->ticket_number,0,-4).'****';
-        $conv->addPage(TicketController::ticket_html($ticket))
-            ->setImageOptions($options)
-            ->toJpg()
-            ->save(public_path().'/assets/tickets/P-'.$tnm.'.jpg');
+            $conv->addPage(TicketController::ticket_html($ticket))
+                ->setImageOptions($options)
+                ->toJpg()
+                ->save(public_path().'/assets/tickets/P-'.$tnm.'.jpg');
         }else{
             $conv->addPage(TicketController::ticket_html($ticket))
                 ->setImageOptions($options)
                 ->toJpg()
                 ->save(public_path().'/assets/tickets/'.$ticket->ticket_number.'.jpg');
-
         }
     }
     public static function ticket_html($ticket)
